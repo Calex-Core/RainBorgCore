7912b7fe9f62269c9a784def2bda493b25a8262f
APIS Project <53349926+APIS-Project@users.noreply.github.com>
2019-08-23 23:47:41 +0300

    RainBorgCore

---
+++ RainBorgCore/Config.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RainBorg
+{
+    class Config
+    {
+        public static async Task Load()
+        {
+            // Check if config file exists and create it if it doesn't
+            if (File.Exists(RainBorg.configFile))
+            {
+                // Load values
+                JObject Config = JObject.Parse(File.ReadAllText(RainBorg.configFile));
+                RainBorg.currencyName = (string)Config["currencyName"];
+                RainBorg.decimalPlaces = (int)Config["decimalPlaces"];
+                RainBorg.databaseFile = (string)Config["databaseFile"];
+                RainBorg.balanceUrl = (string)Config["balanceUrl"];
+                RainBorg.botAddress = (string)Config["botAddress"];
+                RainBorg.botPaymentId = (string)Config["botPaymentId"];
+                RainBorg.successReact = (string)Config["successReact"];
+                RainBorg.tipFee = (decimal)Config["tipFee"];
+                RainBorg.tipMin = (decimal)Config["tipMin"];
+                RainBorg.tipMax = (decimal)Config["tipMax"];
+                RainBorg.userMin = (int)Config["userMin"];
+                RainBorg.userMax = (int)Config["userMax"];
+                RainBorg.waitMin = (int)Config["waitMin"];
+                RainBorg.waitMax = (int)Config["waitMax"];
+                RainBorg.megaTipAmount = (decimal)Config["megaTipAmount"];
+                RainBorg.megaTipChance = (double)Config["megaTipChance"];
+                RainBorg.accountAge = (int)Config["accountAge"];
+                RainBorg.timeoutPeriod = (int)Config["timeoutPeriod"];
+                RainBorg.flushPools = (bool)Config["flushPools"];
+                RainBorg.developerDonations = (bool)Config["developerDonations"];
+                RainBorg.logLevel = (int)Config["logLevel"];
+                RainBorg.logFile = (string)Config["logFile"];
+                RainBorg.botToken = (string)Config["botToken"];
+                RainBorg.botPrefix = (string)Config["botPrefix"];
+                RainBorg.tipPrefix = (string)Config["tipPrefix"];
+                RainBorg.spamWarning = (string)Config["spamWarning"];
+                RainBorg.ChannelWeight = Config["channelWeight"].ToObject<List<ulong>>();
+                RainBorg.StatusChannel = Config["statusChannel"].ToObject<List<ulong>>();
+                RainBorg.wordFilter = Config["wordFilter"].ToObject<List<string>>();
+                RainBorg.requiredRoles = Config["requiredRoles"].ToObject<List<string>>();
+                RainBorg.ignoredNicknames = Config["ignoredNicknames"].ToObject<List<string>>();
+                RainBorg.statusImages = Config["statusImages"].ToObject<List<string>>();
+                RainBorg.donationImages = Config["donationImages"].ToObject<List<string>>();
+                foreach (ulong Id in RainBorg.ChannelWeight)
+                    if (!RainBorg.UserPools.ContainsKey(Id))
+                        RainBorg.UserPools.Add(Id, new List<ulong>());
+            }
+            else await Save();
+        }
+
+        public static Task Save()
+        {
+            // Store values
+            JObject Config = new JObject
+            {
+                ["currencyName"] = RainBorg.currencyName,
+                ["decimalPlaces"] = RainBorg.decimalPlaces,
+                ["databaseFile"] = RainBorg.databaseFile,
+                ["balanceUrl"] = RainBorg.balanceUrl,
+                ["botAddress"] = RainBorg.botAddress,
+                ["botPaymentId"] = RainBorg.botPaymentId,
+                ["successReact"] = RainBorg.successReact,
+                ["tipFee"] = RainBorg.tipFee,
+                ["tipMin"] = RainBorg.tipMin,
+                ["tipMax"] = RainBorg.tipMax,
+                ["userMin"] = RainBorg.userMin,
+                ["userMax"] = RainBorg.userMax,
+                ["waitMin"] = RainBorg.waitMin,
+                ["waitMax"] = RainBorg.waitMax,
+                ["megaTipAmount"] = RainBorg.megaTipAmount,
+                ["megaTipChance"] = RainBorg.megaTipChance,
+                ["accountAge"] = RainBorg.accountAge,
+                ["timeoutPeriod"] = RainBorg.timeoutPeriod,
+                ["flushPools"] = RainBorg.flushPools,
+                ["developerDonations"] = RainBorg.developerDonations,
+                ["logLevel"] = RainBorg.logLevel,
+                ["logFile"] = RainBorg.logFile,
+                ["botToken"] = RainBorg.botToken,
+                ["botPrefix"] = RainBorg.botPrefix,
+                ["tipPrefix"] = RainBorg.tipPrefix,
+                ["spamWarning"] = RainBorg.spamWarning,
+                ["channelWeight"] = JToken.FromObject(RainBorg.ChannelWeight),
+                ["statusChannel"] = JToken.FromObject(RainBorg.StatusChannel),
+                ["wordFilter"] = JToken.FromObject(RainBorg.wordFilter),
+                ["requiredRoles"] = JToken.FromObject(RainBorg.requiredRoles),
+                ["ignoredNicknames"] = JToken.FromObject(RainBorg.ignoredNicknames),
+                ["statusImages"] = JToken.FromObject(RainBorg.statusImages),
+                ["donationImages"] = JToken.FromObject(RainBorg.donationImages)
+            };
+
+            // Flush to file
+            File.WriteAllText(RainBorg.configFile, Config.ToString());
+
+            // Completed
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RainBorgCore/Utilities.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace RainBorg
+{
+    public partial class RainBorg
+    {
+        internal static decimal GetBalance()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string dl = client.DownloadString(balanceUrl);
+                    JObject j = JObject.Parse(dl);
+                    return (decimal)j["balance"];
+                }
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public static decimal Floor(decimal Input)
+        {
+            var r = Convert.ToDecimal(Math.Pow(10, decimalPlaces));
+            return Math.Floor(Input * r) / r;
+        }
+
+        public static string Format(decimal Input)
+        {
+            Input = Floor(Input);
+            string f = "{0:#,##0.#############}";
+            return string.Format(f, Input);
+        }
+        public static string Format(double Input)
+        {
+            decimal I = Floor((decimal)Input);
+            string f = "{0:#,##0.#############}";
+            return string.Format(f, I);
+        }
+
+        // Log
+        public static void Log(string Source, string Message, params object[] Objects)
+        {
+            // Log to console
+            Console.WriteLine("{0} {1}\t{2}", DateTime.Now.ToString("HH:mm:ss"), Source, string.Format(Message, Objects));
+
+            // If log file is specified
+            if (!string.IsNullOrEmpty(logFile))
+                using (StreamWriter w = File.AppendText(logFile))
+                    w.WriteLine(string.Format("{0} {1} {2}\t{3}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString(), Source, string.Format(Message, Objects)));
+        }
+    }
+}
